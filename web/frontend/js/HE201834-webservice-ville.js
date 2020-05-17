284a686d254a5884ff872786e8c1df83cb601d5f
Jju17 <julien.rahier@gmail.com>
2020-05-17 21:03:14 +0200

    add Reviews

--- web/frontend/js/HE201834-webservice-ville.js
+++ web/frontend/js/HE201834-webservice-ville.js
@@ -1,24 +1,33 @@
 "use strict";
 
+let activityChoosen = "";
+
+
+
 // auteur Etolo Gabriel HE201834
 function initPage() {
-	let xhr = new XMLHttpRequest();
-	xhr.open('get', 'code', true);
-        xhr.onload = function() {
-		remplireTable(JSON.parse(this.responseText), 'ville');
-	};
-	xhr.send();
+  initListener();
+
+  let xhr = new XMLHttpRequest();
+  xhr.open('get', 'code', true);
+  xhr.onload = function() {
+    remplireTable(JSON.parse(this.responseText), 'ville');
+  };
+  xhr.send();
 }
 
-function remplireTable(reponseRequete, idBodyTable){
-	let donnees= "";
-	for(let e of reponseRequete){
-	donnees += '<tr>'+ '<td onclick="getVille(\''+e.code+'\');">'+ e.nom+ '</td>'+'</tr>'+'\n';
-	}
-	
-	document.getElementById(idBodyTable).innerHTML= donnees;
-	}
+function remplireTable(reponseRequete, idBodyTable) {
+  let donnees = '';
+  for (let e of reponseRequete) {
+    donnees += '<tr>' + '<td onclick="getVille(\'' + e.code + '\');">' + e.nom + '</td>' + '</tr>' + '\n';
+  }
+
+  document.getElementById(idBodyTable).innerHTML = donnees;
+}
+
+
 //////////////////////////// Auteur Florent Biard HE201813/////////////////////////////
+
 
 function getVille(code) {
   let xhr = new XMLHttpRequest();
@@ -27,10 +36,11 @@ function getVille(code) {
     var ligne = JSON.parse(this.responseText);
     var lng = '';
     for (var i in ligne) {
-      lng +='<tr>' + '<td onclick="getContact(\'' + ligne[i].Nom + '\');">' + ligne[i].Nom + '</td>'
-      + '<td onclick="getContact(\'' + ligne[i].Nom + '\');">' + ligne[i].Type + '</td>' +
-        '<td>' + '<button onclick="toggleForm(\'' + ligne[i].Nom + '\')">Ajouter un avis</button> ' + ' </td>' +
-
+      lng +='<tr>' +
+      '<td onclick="getContact(\'' + ligne[i].Nom + '\');">' + ligne[i].Nom + '</td>' +
+      '<td onclick="getContact(\'' + ligne[i].Nom + '\');">' + ligne[i].Type + '</td>' +
+      '<td>' + '<button onclick="toggleForm(\'' + ligne[i].Nom + '\')">Ajouter un avis</button> ' +
+      ' </td>' +
         '</tr>' + '\n';
     }
     document.getElementById('activite').innerHTML = lng;
@@ -38,22 +48,24 @@ function getVille(code) {
 
   xhr.send();
 }
-	
-	////////////////// auteur Dziemianko Sebastian HE201808 ///////////////////////
+
+
+
+////////////////// auteur Dziemianko Sebastian HE201808 ///////////////////////
 
 
 function getContact(Nom) {
-	getReviews(Nom);
-	let xhr = new XMLHttpRequest();
-	xhr.open('get', 'Contact?acti=' + Nom, true);
-	xhr.onload = function() {
-	document.getElementById('contact').innerHTML = xhr.responseText + '\n';
-			
-	}
-	
-	xhr.send();
+  getReviews(Nom);
+  let xhr = new XMLHttpRequest();
+  xhr.open('get', 'Contact?acti=' + Nom, true);
+  xhr.onload = function() {
+    var reponse = xhr.responseText;
+    document.getElementById('contact').innerHTML = reponse + '\n';
+  }
+  xhr.send();
 
 }
+
 
 ////////////////// auteur Julien Rahier HE201901 ///////////////////////
 
@@ -61,7 +73,16 @@ function getReviews(Nom) {
   let xhr = new XMLHttpRequest();
   xhr.open('get', 'review?acti=' + Nom, true);
   xhr.onload = function() {
-    document.getElementById('reviews').innerHTML = xhr.responseText + '\n';
+    var ligne = JSON.parse(this.responseText);
+    var lng = '';
+    for (var i in ligne) {
+      lng +=
+      '<tr>' +
+        '<td>' + ligne[i].actiReviewTitle + '</td>' +
+        '<td>' + ligne[i].actiReviewContent + '</td>' +
+      '</tr>' + '\n';
+    }
+    document.getElementById('reviews').innerHTML = lng;
   }
 
   xhr.send();
@@ -69,18 +90,22 @@ function getReviews(Nom) {
 }
 
 
-function toggleForm() {
-  var formulaire = document.getElementById('formulaire');
+function toggleForm(Nom) {
+  activityChoosen = Nom;
 
-  if (formulaire.style.display == 'block') {
-    formulaire.style.display = 'none';
+  if (formReview.style.display == 'block') {
+    formReview.style.display = 'none';
   } else {
-    formulaire.style.display = 'block';
+    formReview.style.display = 'block';
   }
 }
 
 
 
-	
-	
 
+
+/*
+
+<link href="https://fonts.googleapis.com/css?family=McLaren|Montserrat&display=swap" rel="stylesheet" />
+
+*/
---
+++ web/frontend/js/HE201901_webservice_form.js
@@ -0,0 +1,30 @@
+"use strict"
+
+function initListener() {
+  let formReview = document.getElementById("formReview");
+  formReview.style.display = 'none';
+  formReview.addEventListener("submit", addReview);
+}
+
+function addReview(event) {
+    event.preventDefault();
+    let formulaire = this;
+    ajout(formulaire.title.value,formulaire.content.value);
+}
+
+function ajout(title, content){
+	let xhr = new XMLHttpRequest();
+  console.log(activityChoosen);
+  console.log(title);
+  console.log(content);
+
+	xhr.open('get', "addReview?acti="+ activityChoosen +"&title=" + title + "&content=" + content, true);
+
+  console.log(xhr);
+	let newAdd = "<tr><td>" + title + "</td><td>" + content +"</td></tr>";
+
+  document.getElementById("reviews").innerHTML = newAdd;
+
+	xhr.send();
+
+}
