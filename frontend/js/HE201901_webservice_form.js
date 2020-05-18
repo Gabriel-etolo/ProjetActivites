68c31c954a2fe266bfc2fdf630169fa620ddbb24
Jju17 <julien.rahier@gmail.com>
2020-05-18 17:07:47 +0200

    JSDoc

--- frontend/js/HE201901_webservice_form.js
+++ frontend/js/HE201901_webservice_form.js
@@ -2,31 +2,27 @@
 
 "use strict"
 
-function initListener() {
+function initListener() { // Initialise le formulaire et ajoute un listener dessus
   let formReview = document.getElementById("formReview");
   formReview.style.display = 'none';
   formReview.addEventListener("submit", addReview);
 }
 
 function addReview(event) {
-    event.preventDefault();
+    event.preventDefault(); // Permet de ne pas recharger la page
     let formulaire = this;
-    ajout(formulaire.title.value,formulaire.content.value);
-    formulaire.reset();
+    ajout(formulaire.title.value,formulaire.content.value); // appelle la fonction ajout avec le titre et le contenu d'un avis
+    formulaire.reset(); // Vide le formulaire des infos
 }
 
 function ajout(title, content){
-	let xhr = new XMLHttpRequest();
-  console.log(activityChoosen);
-  console.log(title);
-  console.log(content);
+	let xhr = new XMLHttpRequest(); // Initialise une requête xhr
 
-	xhr.open('get', "addReview?acti="+ activityChoosen +"&title=" + title + "&content=" + content, true);
+	xhr.open('get', "addReview?acti="+ activityChoosen +"&title=" + title + "&content=" + content, true); // Appelle
 
-  console.log(xhr);
 	let newAdd = "<tr><td>" + title + "</td><td>" + content +"</td></tr>";
 
-  document.getElementById("reviews").innerHTML = getReviews(activityChoosen);
+  document.getElementById("reviews").innerHTML = getReviews(activityChoosen); //Mets à jour les avis
 
 	xhr.send();
 
