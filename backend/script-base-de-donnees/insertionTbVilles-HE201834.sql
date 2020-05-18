07a1c7af1d573ab2daf650d881faded245b3992c
Gabriel-etolo <64024242+Gabriel-etolo@users.noreply.github.com>
2020-05-18 16:10:06 +0200

    Update insertionTbVilles-HE201834.sql

--- backend/script-base-de-donnees/insertionTbVilles-HE201834.sql
+++ backend/script-base-de-donnees/insertionTbVilles-HE201834.sql
@@ -1,13 +1,13 @@
-INSERT INTO tbVilles (VilleCode, VilleNom, VilleRegion)
+INSERT INTO tbVilles (VilleCode, VilleNom)
 VALUES
-('1000','Bruxelles','Bruxelles'),
-('1300','Wavre','Brabant Wallon'),
-('2000','Anvers','Anvers'),
-('3000','Leuven','Brabant Flamand'),
-('3500','Hasselt','Limbourg'),
-('4000','Liege','Liege'),
-('5000','Namur','Namur'),
-('6000','Charleroi','Hainaut'),
-('6600','Bastogne','Luxembourg'),
-('8000','Brugge','Flandre-occidentale'),
-('9000','Gand','Flandre-orientale');
+('1000','Bruxelles'),
+('1300','Wavre'),
+('2000','Anvers'),
+('3000','Leuven'),
+('3500','Hasselt'),
+('4000','Liege'),
+('5000','Namur'),
+('6000','Charleroi'),
+('6600','Bastogne'),
+('8000','Brugge'),
+('9000','Gand');
