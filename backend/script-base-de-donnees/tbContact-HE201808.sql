59b8abed9cc0e51afa060358f52091ff81d86657
DziemiankoSebastian <64025269+DziemiankoSebastian@users.noreply.github.com>
2020-05-18 16:08:48 +0200

    Update tbContact-HE201808.sql

--- backend/script-base-de-donnees/tbContact-HE201808.sql
+++ backend/script-base-de-donnees/tbContact-HE201808.sql
@@ -3,5 +3,8 @@ CREATE TABLE "DBA"."tbContact" (
 	"ActiviteTelephone" CHAR(30) NOT NULL,
 	"ActiviteEmail" CHAR(100) NULL,
 	"Nom" VARCHAR(50) NULL,
+	"idNom" INTEGER NULL,
 	PRIMARY KEY ( "IDContact" ASC )
 ) IN "system";
+
+ALTER TABLE "DBA"."tbContact" ADD CONSTRAINT "tbContact_to_tbActivites" FOREIGN KEY ( "idNom" ASC ) REFERENCES "DBA"."tbActivites" ( "idNom" );
